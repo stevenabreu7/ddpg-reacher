8a74a3db0d1165c3a0dbdb70dfdf754747a3472b
Steven Abreu <stevenabreu7@gmail.com>
2020-05-09 17:22:46 +0200

    + training more models...

--- script.py
+++ script.py
@@ -11,27 +11,27 @@ import sys
 
 env = UnityEnvironment(file_name="src/exec/Reacher1.app")
 seed = 123
-n_episodes = 2
+n_episodes = 1000
 
 agentParams = {
     "actor_arch": [128, 128],
     "critic_arch": [128, 128],
     "buffer_size": int(1e5),
-    "batch_size": 256,
-    "lr_actor": 1e-3,
+    "batch_size": 128,
+    "lr_actor": 1e-4,
     "lr_critic": 1e-3,
-    "gamma": 0.9,
+    "gamma": 0.99,
     "tau": 1e-3,
     "noise_mu": 0.0,
-    "noise_sigma": 0.1,
-    "noise_decay": 0.99,
+    "noise_sigma": 0.2,
+    "noise_decay": 1.0,
     "noise_min_sigma": 0.01,
     "noise_theta": 0.15,
     "weight_decay_critic": 0.0,
     "weight_decay_actor": 0.0,
     "soft_update_freq": 1,
-    "hard_update_at_t": 999,
+    "hard_update_at_t": -1,
     "gradient_clipping": False
 }
-folder = "1"
+folder = "06_given_longer"
 scores = run_ddpg_training(env, agentParams, seed, n_episodes, folder)
